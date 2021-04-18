c62bc3bcf2bfea608cca1047ded6ecc86f7005e2
Peter Hsu <peterhsu3333@gmail.com>
2021-04-18 18:49:24 +0200

    Added untracked files

---
+++ caveat/cache.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cache.h"
+
+void flush_cache( struct cache_t* c )
+{
+  for (int k=0; k<c->ways; k++)
+    memset((char*)c->tags[k], 0, c->ways*sizeof(struct tag_t));
+  memset((char*)c->states, 0, c->rows*sizeof(unsigned short));
+}
+
+void init_cache( struct cache_t* c, struct lru_fsm_t* fsm, int writeable )
+{
+  c->fsm = fsm;			/* note purposely point to [-1] */
+  c->line = 1 << c->lg_line;
+  c->rows = 1 << c->lg_rows;
+  c->ways = fsm->way;
+  c->row_mask = c->rows-1;
+  c->tags = (struct tag_t**)malloc(c->ways*sizeof(struct tag_t**));
+  for (int k=0; k<c->ways; k++)
+    c->tags[k] = (struct tag_t*)malloc(c->rows*sizeof(struct tag_t));
+  c->states = (unsigned short*)malloc(c->rows*sizeof(unsigned short));
+  flush_cache(c);
+  static long place =0;
+  c->evicted = writeable ? &place : 0;
+  c->refs = c->misses = 0;
+  c->updates = c->evictions = 0;
+}
+
+
+void show_cache( struct cache_t* c )
+{
+  fprintf(stderr, "lg_line=%ld lg_rows=%ld line=%ld rows=%ld ways=%ld row_mask=0x%lx\n",
+	  c->lg_line, c->lg_rows, c->line, c->rows, c->ways, c->row_mask);
+}
+
---
+++ caveat/perfctr.c
@@ -0,0 +1,69 @@
+/*
+  Copyright (c) 2020 Peter Hsu.  All Rights Reserved.  See LICENCE file for details.
+*/
+
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "caveat.h"
+#include "opcodes.h"
+#include "insn.h"
+#include "perfctr.h"
+
+
+struct perfCounters_t perf;
+
+
+void perf_create(const char* shm_name)
+{
+  dieif(!shm_name, "Must include --perf=");
+  int n = (insnSpace.bound - insnSpace.base) / 2;
+  long sz = sizeof(struct perf_header_t);
+  sz += n * sizeof(struct count_t);
+  sz += n * sizeof(long) * 3;
+  //  sz += insnSpace.bound - insnSpace.base;
+  int fd = shm_open(shm_name, O_CREAT|O_TRUNC|O_RDWR, S_IRWXU);
+  dieif(fd<0, "shm_open() failed in perf_create");
+  dieif(ftruncate(fd, sz)<0, "ftruncate() failed in perf_create");
+  perf.h = (struct perf_header_t*)mmap(0, sz, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+  dieif(perf.h==0, "mmap() failed in perf_create");
+  memset((char*)perf.h, 0, sz);
+  perf.h->base  = insnSpace.base;
+  perf.h->bound = insnSpace.bound;
+  perf.h->size = sz;
+  perf.count_array = (struct count_t*)( (char*)perf.h + sizeof(struct perf_header_t) );
+  perf.ib_miss = (long*)&perf.count_array[n];
+  perf.ic_miss = (long*)&perf.ib_miss[n];
+  perf.dc_miss = (long*)&perf.ic_miss[n];}
+
+
+void perf_open(const char* shm_name)
+{
+  int fd = shm_open(shm_name, O_RDONLY, 0);
+  dieif(fd<0, "shm_open() failed in perf_open");
+  perf.h = (struct perf_header_t*)mmap(0, sizeof(struct perf_header_t), PROT_READ, MAP_SHARED, fd, 0);    
+  dieif(perf.h==0, "first mmap() failed in perf_open");
+  long sz = perf.h->size;
+  dieif(munmap(perf.h, sizeof(struct perf_header_t))<0, "munmap() failed in perf_open");
+  perf.h = (struct perf_header_t*)mmap(0, sz, PROT_READ, MAP_SHARED, fd, 0);
+  dieif(perf.h==0, "second mmap() failed in perf_open");
+  perf.count_array = (struct count_t*)( (char*)perf.h + sizeof(struct perf_header_t) );
+  long n = (perf.h->bound - perf.h->base) / 2;
+  perf.ib_miss = (long*)&perf.count_array[n];
+  perf.ic_miss = (long*)&perf.ib_miss[n];
+  perf.dc_miss = (long*)&perf.ic_miss[n];
+}
+
+void perf_close()
+{
+  dieif(munmap(perf.h, perf.h->size)<0, "munmap() failed in perf_close");
+}
